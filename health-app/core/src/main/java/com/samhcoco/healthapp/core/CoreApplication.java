67210fc6e0a96c577f73898a6d7803dc9a0ae1a6
Samuel Coco <samuel.coco@gobsmack.co.uk>
2020-10-29 23:41:34 +0000

    Microservices initial setup WIP

--- health-app/core/src/main/java/com/samhcoco/healthapp/core/CoreApplication.java
+++ health-app/core/src/main/java/com/samhcoco/healthapp/core/CoreApplication.java
@@ -2,8 +2,11 @@ package com.samhcoco.healthapp.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.samhcoco")
 public class CoreApplication {
 
     public static void main(String[] args) {
--- health-app/user/src/main/java/com/samhcoco/healthapp/user/UserApplication.java
+++ health-app/user/src/main/java/com/samhcoco/healthapp/user/UserApplication.java
@@ -3,12 +3,14 @@ package com.samhcoco.healthapp.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @EnableCaching
 @EnableSwagger2
 @SpringBootApplication
+@ComponentScan(basePackages = "com.samhcoco")
 public class UserApplication {
 
     public static void main(String[] args) {
