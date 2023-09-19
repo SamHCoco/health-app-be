2cab80b570616d700c17154e7d57a367e2fe38ce
Samuel Coco <Samuel.HenriAC@outlook.com>
2023-09-19 07:26:19 +0100

    Fixed issue with missing main manifest attribute

---
+++ spring-cloud-gateway/src/main/java/com/samhcoco/healthapp/gateway/HealthAppGateway.java
@@ -0,0 +1,13 @@
+package com.samhcoco.healthapp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAppGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthAppGateway.class, args);
+    }
+
+}
