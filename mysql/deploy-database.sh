5ecbb65039cd67595c84058631d62d53a2ed2103
Samuel Coco <Samuel.HenriAC@outlook.com>
2025-02-14 11:02:18 +0000

    Master branch clean-up

--- mysql/deploy-database.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-namespace=health-app
-
-kubectl apply -f namespace.yaml
-kubectl apply -f mysql-config.yaml -n ${namespace}
-kubectl apply -f mysql-secret.yaml -n ${namespace}
-kubectl apply -f statefulset.yaml -n ${namespace}
