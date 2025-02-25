b693f7a8ef84412befd116a48b8392fc96d157e2
Samuel Coco <Samuel.HenriAC@outlook.com>
2025-02-25 12:52:02 +0000

    User-Service application yaml modified

--- user/src/main/java/com/samhcoco/healthapp/user/UserApplication.java
+++ user/src/main/java/com/samhcoco/healthapp/user/UserApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @EnableCaching
-@EnableSwagger2
 @SpringBootApplication
 @ComponentScan(basePackages = "com.samhcoco")
 public class UserApplication {
